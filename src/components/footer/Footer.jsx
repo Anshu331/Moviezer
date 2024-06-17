77e27ccb7766c5c6d958bff6aa357f10003c1d97
Anshu Kumar Bishwas <123227226+Anshu331@users.noreply.github.com>
2024-06-17 22:58:02 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Welcome to Moviezer, your ultimate hub for all things movies and web
+          series. Explore a vast collection of films and shows from around the
+          world, complete with detailed information, user reviews, and
+          personalized recommendations. Stay updated with the latest news,
+          trailers, and exclusive content, and manage your watchlist
+          effortlessly. Dive into the world of entertainment with Moviezer and
+          discover your next favorite show today!
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
